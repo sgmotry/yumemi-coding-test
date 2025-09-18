2574b8ca9f9cb0c4f1e7c83451dbaa5d17165407
ryota <mc_ryouta3@yahoo.co.jp>
2025-09-18 14:16:55 +0900

    formatの実行

--- src/app/components/PrefectureCheckBox.tsx
+++ src/app/components/PrefectureCheckBox.tsx
@@ -1,28 +1,29 @@
 'use client'
 import { Prefecture } from '@/app/types/types'
-import { useState } from 'react';
+import { useState } from 'react'
 
 const PrefectureCheckBox = ({ data }: { data: Prefecture[] }) => {
-  const [checkedCode, setCheckedCode] = useState<number[]>([]);
+  const [checkedCode, setCheckedCode] = useState<number[]>([])
 
   const handleCheckboxChange = (target: number) => {
     if (checkedCode.includes(target)) {
-      setCheckedCode(checkedCode.filter((code) => code !== target));
+      setCheckedCode(checkedCode.filter((code) => code !== target))
     } else {
-      setCheckedCode([...checkedCode, target]);
+      setCheckedCode([...checkedCode, target])
     }
-  };
+  }
 
   return (
     <>
-      <div className='grid grid-cols-7'>
+      <div className="grid grid-cols-7">
         {data.map((pref) => (
           <div key={pref.prefCode}>
             <label>
               <input
                 type="checkbox"
                 checked={checkedCode.includes(pref.prefCode)}
-                onChange={() => handleCheckboxChange(pref.prefCode)} />
+                onChange={() => handleCheckboxChange(pref.prefCode)}
+              />
               {pref.prefName}
             </label>
           </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
 
   return (
     <>
-      <PrefectureCheckBox data={prefectureData}/>
+      <PrefectureCheckBox data={prefectureData} />
     </>
   )
 }
