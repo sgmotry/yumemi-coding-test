7beedefff8ed297b20fee14131878fd434ed080a
ryota <mc_ryouta3@yahoo.co.jp>
2025-09-18 15:12:46 +0900

    人口構成用のタイプ宣言

--- src/app/types/types.ts
+++ src/app/types/types.ts
@@ -7,3 +7,20 @@ export type PrefectureResponse = {
   message: string
   result: Prefecture[]
 }
+
+export type Population = {
+  boundaryYear: number
+  data: {
+    label: string
+    data: {
+      year: number
+      value: number
+      rate: number
+    }[]
+  }[]
+}
+
+export type PopulationResponse = {
+  message: string
+  result: Population
+}
