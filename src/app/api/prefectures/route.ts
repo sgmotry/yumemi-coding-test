31c13d09e1aa36bd8eef7f0d3b36647444a04335
sgmotry <45906970+sgmotry@users.noreply.github.com>
2025-09-19 16:01:54 +0900

    クライアントサイドでのAPIキー隠ぺいのため、route.tsをそれぞれ作成

---
+++ src/app/api/prefectures/route.ts
@@ -0,0 +1,32 @@
+import { PrefectureResponse } from '@/app/types/types'
+
+export async function GET() {
+  const APIKEY = process.env.YUMEMI_API_KEY
+
+  const response = await fetch(
+    'https://yumemi-frontend-engineer-codecheck-api.vercel.app/api/v1/prefectures',
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'X-API-KEY': APIKEY || '',
+      },
+    },
+  )
+
+  if (!response.ok) {
+    return new Response(
+      JSON.stringify({ error: 'Failed to fetch prefecture data' }),
+      {
+        status: response.status,
+        headers: { 'Content-Type': 'application/json' },
+      },
+    )
+  }
+
+  const data: PrefectureResponse = await response.json()
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: { 'Content-Type': 'application/json' },
+  })
+}
