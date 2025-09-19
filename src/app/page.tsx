8e8e92e727b46e13f3231929d6d6823a2fc51126
sgmotry <45906970+sgmotry@users.noreply.github.com>
2025-09-20 02:59:55 +0900

    /ページのインポート修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import PopulationTrendApp from "./components/PopulationTrendApp"
+import PopulationTrendApp from "../components/PopulationTrendApp"
 
 export default async function Home() {
   return (
