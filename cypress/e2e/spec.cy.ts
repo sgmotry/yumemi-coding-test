0b3f9a2d911acd1d6e86bab6213771281f3bdd71
sgmotry <45906970+sgmotry@users.noreply.github.com>
2025-09-20 08:13:17 +0900

    cypress導入、設定

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,13 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000')
+    cy.contains('北海道').click()
+    cy.contains('福井県').click()
+    cy.contains('愛知県').click()
+    cy.contains('年少人口').click()
+    cy.contains('生産年齢人口').click()
+    cy.contains('老年人口').click()
+    cy.contains('割合').click()
+    cy.contains('総人口').click()
+  })
+})
